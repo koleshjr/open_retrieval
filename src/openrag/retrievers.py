69c13e0ac41fd720754aca72751d20a51c3f8d7e
koleshjr <stevekolesh975@gmail.com>
2024-05-24 13:15:21 +0300

    adds working tests

--- src/openrag/retrievers.py
+++ src/openrag/retrievers.py
@@ -23,7 +23,9 @@ class Retriever:
         docs =self.vector_database.similarity_search(query=query, k=top_k, filter = filter)
         data = self.reranker.rank(query = query, docs = [doc.page_content for doc in docs])
         # Extract the list of Result objects
+        print(data)
         results = next(item for item in data if item[0] == 'results')[1]
+        print(results)
 
         # Sort results by rank
         sorted_results = sorted(results, key=lambda x: x.rank)
