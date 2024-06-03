6b57d6134ba2dc7a6cf781e16d1228cb28699831
Kolesh jr <50013373+koleshjr@users.noreply.github.com>
2024-06-03 17:02:59 +0300

    Update test_document_loaders.py

--- tests/test_document_loaders.py
+++ tests/test_document_loaders.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from src.open_rag.document_loaders import DocumentLoader
+from src.open_retrieval.document_loaders import DocumentLoader
 
 class TestDocumentLoader:
     @pytest.fixture
